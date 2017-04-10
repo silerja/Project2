d589f1b0b9db053db274193f8a18f6797abb9c7d
Joseph Siler <Joseph Siler>
2017-04-10 10:09:57 -0400

    Func not working

---
+++ index2.js
@@ -0,0 +1,17 @@
+	var questions = new Array();
+	//First question
+	questions[0] = 'Who is this?'
+	//second question
+	//questions[1] = 'Who do you see in the picture?'
+    
+	var choices = new Array();
+	choices[0] = ['Joe Siler' , 'Cordell McCurry', 'Scott Layne'],
+		
+	var answers = new Array();
+	answers[0] = 'Joe Siler'
+
+//(function() {
+  //var questions = [{
+    //question: 'Who is this',
+    //choices: ['Joe Siler','Cordell McCurry', 'Scott Layne'],
+    //correctAnswer: 'Joe Siler'
--- spec/quiz_spec.js
+++ spec/quiz_spec.js
@@ -1,8 +1,8 @@
 //import code for quiz to test
-var quiz = require("..index");
+var quiz = require("../index2");
 
 describe("The answer to the first question 'Who is this?'.")
-	if( choice = Joe Siler) {
+	if( choice = 'Joe Siler') {
 		greeting = "Correct";
 	} else {
 		greeting = "Incorrect";
