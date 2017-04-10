8d6cde9686074f25e961888f2bfc5ec0a23e1861
Joseph Siler <Joseph Siler>
2017-04-10 11:00:36 -0400

    tested func

--- index2.js
+++ index2.js
@@ -1,18 +1,5 @@
-	var questions = new Array();
-	//First question
-	questions[0] = 'Who is this?'
-	//second question
-	//questions[1] = 'Who do you see in the picture?'
-    
-	var choices = new Array();
-	choices[0] = ['Joe Siler' , 'Cordell McCurry', 'Scott Layne'],
-		
-	var answers = new Array();
-	answers[0] = 'Joe Siler'
-
-//(function() {
-  //var questions = [{
-    //question: 'Who is this',
-    //choices: ['Joe Siler','Cordell McCurry', 'Scott Layne'],
-    //correctAnswer: 'Joe Siler'
-	//}];
+//quiz function
+ 	var question =
+    question = 'Who is this?',
+    choices = ['Joe Siler','Cordell McCurry', 'Scott Layne'],
+    correctAnswer = 'Joe Siler'
--- spec/quiz_spec.js
+++ spec/quiz_spec.js
@@ -1,9 +1,17 @@
-//import code for quiz to test
-var quiz = require("../index2");
+//import code from index to test
+var quiz = require('../index2');
 
-describe("The answer to the first question 'Who is this?'.")
-	if( choice = 'Joe Siler') {
-		greeting = "Correct";
-	} else {
-		greeting = "Incorrect";
-	};
+describe('The answer to the first question "Who is this?"', () => {
+
+	it('can blow smoke', () => {
+		expect(true).toBe(true);
+	});
+
+
+	it('can tell you if you are right or wrong', () => {
+		expect('Joe Siler').toBe(true);
+		//expect('Cordell McCurry').toBe(false);
+		//expect('Scott Layne').toBe(false);
+	});
+
+});
